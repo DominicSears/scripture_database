26ae6410167016139268a0b6a3d37f0107ec6b3a
Dominic Sears <dominicsears@hotmail.com>
2017-11-18 13:02:22 -0800

    Added more conditions to adding the forms to the database

--- server_script/newrow.php
+++ server_script/newrow.php
@@ -4,7 +4,7 @@
     $conn = getConnection();
     $setS = $conn->prepare("INSERT INTO `scripture` (`book`, `chapter`, `verse`, `tags`, `comments`, `explanation`, `referenced_in`, `included_in_FP`) VALUES (?, ?, ?, ?, ?, ?, ?, ?);");
     
-    if (isset($_POST['book'])){
+    if (isset($_POST['book']) and isset($_POST['chapter']) and isset($_POST['verse']) and isset($_POST['explanation']) and isset($_POST['tags'])){
         try {
             $setS->execute(array($_POST['book'], $_POST['chapter'], $_POST['verse'], $_POST['tags'], $_POST['comments'], $_POST['explanation'], $_POST['referenced_in'], $_POST['in_FP']));
         } catch (PDOException $e) {
